cc56f0645dc7f95b40a0d01220a4479bb04f5c63
Matt Matuszak <mattma2zak@gmail.com>
2017-02-03 15:17:06 -0600

    to 48

--- src/main/resources/scripts/convertToMono.sh
+++ src/main/resources/scripts/convertToMono.sh
@@ -21,8 +21,8 @@ echo "Target Audio:" $targetAudio
 
 echo ""
 echo "going down to MONO and reducing to a 48 bit rate..."
-lame --mp3input --silent -m m -b 32 $sourceAudio $targetAudio
-echo "Finished going down to MONO and reducing to a 32 buffer rate."
+lame --mp3input --silent -m m -b 48 $sourceAudio $targetAudio
+echo "Finished going down to MONO and reducing to a 48 buffer rate."
 echo ""
 
 exit 0
